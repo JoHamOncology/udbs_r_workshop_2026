2cfb3d5de78bc27540efa689ff87e846314f7380
Joelle <jphamilt@uab.edu>
2026-01-29 15:24:10 -0600

    adding first R script

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # udbs_r_workshop_2026
 repository for the UBDS R workshop to have our shell scripts and R code 
 THis repository contains scripts, data, R code for the RO workshop o attended 1/29/26 
+
+## Directories
+- **shell_scripts/** - Bash scripts from workshop
+- **analysis/** code and data for R workshop
---
+++ analysis/workshop R.R
@@ -0,0 +1,30 @@
+#this is a comment
+100 + 101
+3+5*10
+
+# help me remember order of operations 
+3+(5*10)
+
+# EXPONENT  ^
+3^2
+
+# boolean are these equal?
+10 == 10
+9==10 
+
+# computers do not know decimals
+10.01 == 10.
+
+
+# fractions - what do they look like will use e 
+2/10000
+50000000/2
+
+
+# R has functions - operations already prefined
+getwd()
+sin(1)
+
+
+x <- 1/40
+x = 1/100
